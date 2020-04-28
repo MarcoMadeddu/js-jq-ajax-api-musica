9c4f714d2cfcd353cb6be32940b7fc6df3a94b95
MarcoMadeddu <marcomade99@gmail.com>
2020-04-28 17:50:33 +0200

    fix and add comments

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
+
+	// V A R I A B I L I //
 	var myApi = "https://flynn.boolean.careers/exercises/api/array/music";
 	var container = $(".cds-container");
 	var source = $("#template").html();
 	var template = Handlebars.compile(source);
 
+	// C H I A M A T A  A P I //
 	$.ajax({
 		url: myApi,
 		method: "GET",
 
 		success: function(data){
 			var songDesc = data.response;
-
 			for(var i = 0; i< songDesc.length; i++){
+
 				var generateSong={
 					Img: songDesc[i].poster,
 					title: songDesc[i].title,
@@ -22,21 +25,17 @@ $(document).ready(function() {
 
 				var set = template(generateSong);
 				container.append(set);
-
-				
 			}
-			
 		},
 
 		error: function(){
 			console.log("oh no:(");
-			
 		},
 	})
 
+	// R I C E R C A  I N  B A S E  A L  G E N E R E //
 	var search = $(".button");
 	var select = $("#music-gen");
-	var selected = select.children("option:selected").val();
 
 	search.click(function(){
 		var selected = select.children("option:selected").val();
@@ -44,14 +43,9 @@ $(document).ready(function() {
 		cds.hide();
 		
 		cds.each(function(){
-
 			if($(this).find(".gen").text() == selected){
 				$(this).show();
 			}
 		})
-		
 	})
-	
-	
-	
 });
